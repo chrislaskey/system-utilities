0453f15d5121c70c993ab1b102809c0d62db437a
Chris Laskey <contact@chrislaskey.com>
2013-12-17 11:18:21 -0500

    Added RedHat VM Networking fix

---
+++ fix-networking-on-redhat-vms.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# When cloning virtual machines the hypervisor will change the VMs MAC address
+# automatically. RedHat distributions store MAC address information and bind
+# network interfaces to them. This causes the ethX number to increase each time
+# the system is cloned.
+#
+# This script fixes this behavior by:
+# - Removing the MAC address information from the kernel
+# - Removing MAC and UUID addresses from eth0 configuration file
+# - Installing this script as an init script to be executed on system
+#   start/stop/resets.
+#
+# Combined this ensures networking on all cloned VMs will work automatically.
+#
+# From: http://www.envision-systems.com.au/blog/2012/09/21/fix-eth0-network-interface-when-cloning-redhat-centos-or-scientific-virtual-machines-using-oracle-virtualbox-or-vmware/
+
+kernel_rules="/etc/udev/rules.d/70-persistent-net.rules"
+eth0_cfg="/etc/sysconfig/network-scripts/ifcfg-eth0"
+
+init_file="/etc/rc.d/init.d/fixVirtualMachineNetworking.sh"
+init_up="/etc/rc0.d/S12fixVirtualMachineNetworking.sh"
+init_down="/etc/rc6.d/S12fixVirtualMachineNetworking.sh"
+
+# Fix networking files
+
+if test -f "$kernel_rules"; then
+	rm -rf "$kernel_rules"
+fi
+
+if test -f "$eth0_cfg"; then
+	sed -i'' -e '/HWADDR/d' "$eth0_cfg"
+	sed -i'' -e '/UUID/d' "$eth0_cfg"
+	grep "# Note:" "$eth0_cfg" || echo "# Note: HWADDR and UUID values automatically removed on reboot by ${init_file}" >> "$eth0_cfg"
+fi
+
+# Install as init script
+
+if ! test -f "$init_file"; then
+	mv "$0" "$init_file"
+fi
+
+if ! test -L "$init_up"; then
+	ln -s "$init_file" "$init_up"
+fi
+
+if ! test -L "$init_down"; then
+	ln -s "$init_file" "$init_down"
+fi
